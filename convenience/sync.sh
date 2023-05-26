337b59bf2595c6e6d82806a0c5b5829a1f8d20ae
dev-mute <dev-mute@users.noreply.github.com>
2023-05-25 22:42:58 -0400

    Added sync script

---
+++ convenience/sync.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+# "Convenience" sync script to update files in this repo with local modifications to ~/.config/nvim
+
+# Check if this script is being run in the "convenience" directory
+if ! [[ "$PWD" = */convenience ]]; then
+    echo '[-] Please run the installation script within the "convenience" directory: cd convenience && ./sync.sh'
+    exit 1
+fi
+
+# Check if existing nvim config is present
+if [ -f ~/.config/nvim/init.vim ] || [ -f ~/.config/nvim/init.lua ]; then
+    echo '[+] Existing nvim init config file found in ~/.config/nvim/.'
+fi
+
+echo '[+] Copying ~/.config/nvim/ to ../nvim/.'
+cp -r ~/.config/nvim/* ../nvim/
